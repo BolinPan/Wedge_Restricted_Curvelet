b710f7b35b1b3cf0f3b4612031d6e7473e0fd11b
BolinPan <poon19910731@gmail.com>
2021-05-27 18:20:10 +0100

    Add files via upload

--- example_restrictedCurvelet_3D.m
+++ example_restrictedCurvelet_3D.m
@@ -4,15 +4,15 @@
 % This script shows the restricted Curvelet transform on 3D ball phantom.
 %
 %
-% Copy right (C) 2021 Bolin Pan
+% Copy right (C) 2021 Bolin Pan & Marta M. Betcke
 
 clear all; close all; clc;
 
 
-%% generate image
-Nx = 64;
-Ny = 64;
-Nz = 64;
+%% define 3D image with the same size as 3D data presented in our Curvelet paper
+Nx = 144;
+Ny = 133;
+Nz = 390;
 
 % generate 3D ball phantom
 radius = Nx/4;
@@ -26,8 +26,8 @@ title('$p0$','Interpreter','latex');colorbar;axis image
 
 %% construct wedge restricted Curvelet transform
 cs_transform.class         = 'curvelet'; 
-cs_transform.nbscales      = 3; % number of scales
-cs_transform.nbdstz_coarse = 16; % coarse angles
+cs_transform.nbscales      = 4; % number of scales
+cs_transform.nbdstz_coarse = 42; % coarse angles
 cs_transform.type          = 'p0R'; % used for initial pressure
 
 % constructs the sparsifying transform Psi - data
@@ -39,12 +39,9 @@ cs_transform.constructor = str2func(['@(T) ' cs_transform.class 'CSTransform3D_(
 [cs_transform, cs_transform_tag] = cs_transform.constructor(cs_transform);
 
 % construct restricted Curvelet transform
-csType = 'explicit';
-rCurvelets = [];
-rCurvelets{2} = 1:length(cs_transform.maskC{2}); % remove all wedges in 2nd scale (for visualization purpose)
-rCurvelets{3} = [];
+csType = 'explicit'; % remove same wedges for 3D data presented on the paper
 cs_transform_ = cs_transform;
-cs_transform_.maskC = restrictCurvelet3D(cs_transform_.S,rCurvelets,csType); % construct Curvelet mask 4 5 for 32
+cs_transform_.maskC = restrictCurvelet3D(cs_transform_.S,csType); % construct Curvelet mask 4 5 for 32
 [cs_transform_, cs_transform_data_tag_] = cs_transform_.constructor(cs_transform_); % Update transform. This needs to be improved to construct in one go. 
 
 
@@ -56,17 +53,25 @@ C_ = cs_transform_.unvectPsi(cs_transform_.Psi(phantom)); % restricted Curvelet
 p0Recon = cs_transform.iPsi(cs_transform.vectPsi(C));
 p0Recon_ = cs_transform_.iPsi(cs_transform_.vectPsi(C_));
 
+% compute residual
+r = p0Recon-phantom;
+r_ = p0Recon_-phantom;
+
 
 %% display and compare
+% reconstruction vs phantom, maximum intensity plots
 figure;
-subplot(2,2,1);imagesc(max(abs(permute(p0Recon(:,:,:),[2 3 1])),[],3));colorbar; axis off;axis square;
-title('$\Psi^T \Psi p0$','Interpreter','latex');colorbar;axis image
-subplot(2,2,2);imagesc(max(abs(permute(p0Recon_(:,:,:),[2 3 1])),[],3));colorbar; axis off;axis square;
-title('$\tilde{\Psi}^\dagger\tilde{\Psi}p0$','Interpreter','latex');colorbar;axis image
-subplot(2,2,3);imagesc(max(abs(permute(p0Recon-phantom(:,:,:),[2 3 1])),[],3));colorbar; axis off;axis square;
-title('$\Psi^T \Psi p0 - p0$','Interpreter','latex');colorbar;axis image
-subplot(2,2,4);imagesc(max(abs(permute(p0Recon-phantom(:,:,:),[2 3 1])),[],3));colorbar; axis off;axis square;
-title('$\tilde{\Psi}^\dagger\tilde{\Psi}p0 - p0$','Interpreter','latex');colorbar;axis image
+subplot(2,2,1);imagesc(max(abs(permute(p0Recon(:,:,:),[2 3 1])),[],3));colorbar;axis image;title('$\Psi^T \Psi p0$','Interpreter','latex');
+subplot(2,2,2);imagesc(max(abs(permute(p0Recon_(:,:,:),[2 3 1])),[],3));colorbar;axis image;title('$\tilde{\Psi}^\dagger\tilde{\Psi}p0$','Interpreter','latex');
+subplot(2,2,3);imagesc(max(abs(permute(p0Recon-phantom(:,:,:),[2 3 1])),[],3));colorbar;axis image;title('$\Psi^T \Psi p0 - p0$','Interpreter','latex');
+subplot(2,2,4);imagesc(max(abs(permute(p0Recon_-phantom(:,:,:),[2 3 1])),[],3));colorbar;axis image;title('$\tilde{\Psi}^\dagger\tilde{\Psi}p0 - p0$','Interpreter','latex');
+
+% wedge resctired curvelet reconstruction slice plots
+subplot(2,2,1);imagesc(max(abs(permute(p0Recon_(:,:,:),[2 3 1])),[],3));colorbar;axis image;title('$\Psi^T \Psi p0$','Interpreter','latex');
+subplot(2,2,2);imagesc(squeeze(r_(:,:,85)));colorbar;axis image;title('$\Psi^T \Psi p0 - p0, x_{85}$','Interpreter','latex');
+subplot(2,2,3);imagesc(squeeze(r_(:,59,:)));colorbar;axis image;title('$\tilde{\Psi}^\dagger\tilde{\Psi}p0 - p0, y_{59}$','Interpreter','latex');
+subplot(2,2,4);imagesc(squeeze(r_(59,:,:)));colorbar;axis image;title('$\tilde{\Psi}^\dagger\tilde{\Psi}p0 - p0, z_{59}$','Interpreter','latex');
+
 
 
 
